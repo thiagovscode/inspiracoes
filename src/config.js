07945afc155288d9c80836c0f1a3234ddc8a9abd
Thiago Vasconcelos <southiagovasconcelos@gmail.com>
2026-08-14 00:14:19 -0300

    first commit

--- src/config.js
+++ src/config.js
@@ -5,28 +5,28 @@ export const pageConfig = {
     {
       id: "padrinhos",
       title: "Padrinhos",
-      description: "Cor, ternos e estilo para os padrinhos",
+      description: "Guia de cores, trajes e estilo para os padrinhos",
       url: "https://br.pinterest.com/search/pins/?q=groomsmen%20inspiration",
       image: "https://images.unsplash.com/photo-1510926510493-edfb8f0019fa?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
     },
     {
       id: "madrinhas",
       title: "Madrinhas",
-      description: "Paleta de cores e modelos de vestidos",
+      description: "Guia de cores, vestidos e estilo para as madrinhas",
       url: "https://br.pinterest.com/search/pins/?q=bridesmaid%20dresses",
       image: "https://images.unsplash.com/photo-1566737236500-c8ac43014a67?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
     },
     {
       id: "madmoselles",
       title: "Madmoselles",
-      description: "Cor, vestido e estilo",
+      description: "Guia de cores, vestidos e estilo para as madmoselles",
       url: "https://br.pinterest.com/search/pins/?q=mademoiselle%20dress%20wedding",
       image: "https://images.unsplash.com/photo-1519741497674-611481863552?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
     },
     {
       id: "pais",
       title: "Pais",
-      description: "Elegância e trajes para os pais dos noivos",
+      description: "Guia de cores, trajes e estilo para os pais",
       url: "https://br.pinterest.com/search/pins/?q=parents%20of%20the%20bride%20and%20groom",
       image: "https://images.unsplash.com/photo-1520854221256-17451cc331bf?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
     }
