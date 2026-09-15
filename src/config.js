e1b9650442afcbdc3d440d65a56b94c612635b22
Thiago Vasconcelos <southiagovasconcelos@gmail.com>
2026-09-14 21:50:47 -0300

    feat: ajuste card local

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ export const pageConfig = {
     title: "Onde e Quando",
     location: "Espaço Balboa em Mairiporã",
     address: "Av. Parque Petrópolis, 250 - Serra da Cantareira",
-    time: "15h",
+    time: "14h",
     mapLink: "https://www.google.com/maps/search/?api=1&query=Espa%C3%A7o+Balboa+Mairipor%C3%A3",
     wazeLink: "https://waze.com/ul?q=Espa%C3%A7o%20Balboa%20Mairipor%C3%A3&navigate=yes",
     image: "https://images.unsplash.com/photo-1469371670807-013ccf25f16a?ixlib=rb-4.0.3&auto=format&fit=crop&w=1200&q=80"
@@ -16,35 +16,35 @@ export const pageConfig = {
       title: "Padrinhos",
       description: "Guia de cores, trajes e estilo para os padrinhos",
       url: "https://br.pinterest.com/batistatainara293/inspira%C3%A7%C3%A3o-terno-cinza-claro/?invite_code=da4e648829d84ace8b2cdec2b56a40ac&sender=654711001987632234",
-      image: "https://images.unsplash.com/photo-1593030761757-71fae45fa0e7?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
+      image: "https://i.pinimg.com/736x/60/0c/f8/600cf8a2a7c67533b472a5f88d295a61.jpg",
     },
     {
       id: "madrinhas",
       title: "Madrinhas",
       description: "Guia de cores, vestidos e estilo para as madrinhas",
       url: "https://br.pinterest.com/batistatainara293/inspira%C3%A7%C3%A3o-tons-de-pink/?invite_code=8ef213b2033b405b96407bd080788cbb&sender=654711001987632234",
-      image: "https://images.unsplash.com/photo-1566737236500-c8ac43014a67?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
+      image: "https://i.pinimg.com/1200x/a8/72/5d/a8725d03423275b743b1f2f47d48cbab.jpg",
     },
     {
       id: "demoiselles",
       title: "Demoiselles",
       description: "Guia de cores, vestidos e estilo para as demoiselles",
       url: "https://br.pinterest.com/batistatainara293/inspira%C3%A7%C3%A3o-cor-mademoiselle/?invite_code=f3cd7943cb20487aa3014dcf76742bbf&sender=654711001987632234",
-      image: "https://images.unsplash.com/photo-1519741497674-611481863552?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
+      image: "https://i.pinimg.com/736x/7a/1d/4b/7a1d4bf9226b0795a6f3259867104746.jpg",
     },
     {
       id: "pais",
       title: "Pais",
       description: "Guia de cores, trajes e estilo para os pais",
       url: "https://br.pinterest.com/batistatainara293/inspira%C3%A7%C3%A3o-pais/?invite_code=ae9e90d87640401f97f068bc9966f2a3&sender=654711001987632234",
-      image: "https://images.unsplash.com/photo-1520854221256-17451cc331bf?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
+      image: "https://i.pinimg.com/1200x/d4/d4/cd/d4d4cd1b969840e68fb5d9edc4b72384.jpg",
     },
     {
       id: "daminhas-pajens",
       title: "Daminhas e Pajens",
       description: "Guia de cores, trajes e estilo para as daminhas e pajens",
       url: "https://br.pinterest.com/batistatainara293/inspira%C3%A7%C3%A3o-daminha/?invite_code=dc3a79a051fb4e7babb931c7f518e2e5&sender=654711001987632234",
-      image: "https://images.unsplash.com/photo-1519225421980-715cb0215aed?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
+      image: "https://i.pinimg.com/736x/b8/62/ee/b862ee67c0d4b888603a3a319b372316.jpg",
     }
   ]
 };
